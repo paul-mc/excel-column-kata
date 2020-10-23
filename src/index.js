f604251bd39173f0b28dfc1d2bbd73c080f7f82f
Paul McManus <paul.mcmanus.uk@gmail.com>
2020-10-23 22:30:09 +0100

    Add a solution to the Excel Column kata.
    Create a function that converts an Excel column name ('A', 'AA', 'ZZ')
    into a zero-indexed number.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-export const main = (input) => input;
+export const solution = (letters) => {
+  if (typeof letters !== 'string') {
+    throw new Error('Input is not a string');
+  }
+
+  const lettersOnly = RegExp('^[a-zA-Z]+$');
+  if (!lettersOnly.test(letters)) {
+    throw new Error('Input contains unexpected characters');
+  }
+
+  const values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  const chars = letters.toUpperCase().split('');
+  const charValues = chars.map(
+    (letter) => values.indexOf(letter),
+  );
+
+  const value = charValues.reverse().reduce(
+    (acc, v, index) => (
+      index === 0
+        ? v
+        : acc + (v + 1) * (26 ** index)
+    ),
+    0,
+  );
+
+  return value;
+};
 
 export default {
-  main,
+  solution,
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,42 @@
-import { main } from './index';
+import { solution } from './index';
 
-describe('The `main` function', () => {
-  it('Returns the input string', () => {
-    expect(main('aaa')).toBe('aaa');
+describe('Solution', () => {
+  const testCases = [
+    { input: 'A', expected: 0 },
+    { input: 'B', expected: 1 },
+    { input: 'Z', expected: 25 },
+    { input: 'AA', expected: 26 },
+    { input: 'AB', expected: 27 },
+    { input: 'BA', expected: 52 },
+    { input: 'BZ', expected: 77 },
+    { input: 'CA', expected: 78 },
+    { input: 'ZZ', expected: 701 },
+    { input: 'AAA', expected: 702 },
+  ];
+
+  testCases.forEach((testCase) => {
+    it(`should convert ${testCase.input} to ${testCase.expected}`, () => {
+      expect(solution(testCase.input)).toBe(testCase.expected);
+    });
+  });
+
+  it('should handle lower case strings', () => {
+    expect(solution('a')).toBe(0);
+  });
+
+  it('throws an error for non-string input', () => {
+    try {
+      solution(23);
+    } catch (e) {
+      expect(e).toStrictEqual(new Error('Input is not a string'));
+    }
+  });
+
+  it('throws an error for string input that contains unexpected characters', () => {
+    try {
+      solution('A4B');
+    } catch (e) {
+      expect(e).toStrictEqual(new Error('Input contains unexpected characters'));
+    }
   });
 });
